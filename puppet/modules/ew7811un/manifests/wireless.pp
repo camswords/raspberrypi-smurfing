809d0326012886e10a9b571752ea9574439c5fa7
Cam Swords <ext-cameron.swords@nokia.com>
2012-07-15 22:35:32 +0100

    added a module that will install the wireless ew7811un usb card.

---
+++ puppet/modules/ew7811un/manifests/wireless.pp
@@ -0,0 +1,36 @@
+
+class ew7811un::wireless {
+
+  file { '/lib/modules/3.1.9+/kernel/net/wireless/8192cu.ko':
+    source => '/files/lib/modules/3.1.9+/kernel/net/wireless/8192cu.ko',
+    ensure => present,
+    mode   => 644
+    notify => Exec['depmod -a']
+  }
+
+  exec { 'depmod -a' }
+
+  file { '/etc/modprobe.d/blacklist.conf':
+    source => '/files/etc/modprobe.d/blacklist.conf',
+    ensure => present,
+    mode   => 644
+  }
+
+  file { '/etc/modules':
+    source => '/file/etc/modules',
+    ensure => present,
+    mode   => 644
+  }
+
+  file { '/etc/wpa_supplicant.conf':
+    source => '/files/etc/wpa_supplicant.conf',
+    ensure => present,
+    mode   => 644
+  }
+
+  file { '/etc/network/interfaces':
+    source => '/files/etc/network/interfaces',
+    ensure => present,
+    mode   => 644
+  }
+}
